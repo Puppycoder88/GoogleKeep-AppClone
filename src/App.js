5d9e361cb289996e300addee1ecfb92b23b94259
Jaingarima08 <g.jain@gembainfotech.com>
2025-01-27 09:50:42 +0530

    googlekeep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Header";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
+import Footer from "./Footer";
 
-function App() {
+const App = () => {
+  const [addItem, setAddItem] = useState([]);
+
+  const addNote = (note) => {
+    // alert("I am clicked")
+    setAddItem((prevData) => {
+      return [...prevData, note];
+    });
+    console.log(note);
+  };
+
+  const onDelete = (id) => {
+    setAddItem((olddata) =>
+      olddata.filter((currdata, indx) => {
+        return indx !== id;
+      })
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Header />
+      <CreateNote passNote={addNote} />
 
+      {addItem.map((val, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={val.title}
+            content={val.content}
+            DeleteItem={onDelete}
+          />
+        );
+      })}
+      <Footer />
+    </>
+  );
+};
 export default App;
